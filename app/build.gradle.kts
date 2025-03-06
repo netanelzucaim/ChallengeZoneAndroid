2a630413c97b2749b1b86eae3a631b8d9ee3f201
Your Name <you@example.com>
2025-03-07 01:44:03 +0200

    working chat

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,6 @@ android {
         buildConfigField("String", "CLOUD_NAME", "\"${project.properties["CLOUD_NAME"] ?: ""}\"")
         buildConfigField("String", "API_KEY", "\"${project.properties["API_KEY"] ?: ""}\"")
         buildConfigField("String", "API_SECRET", "\"${project.properties["API_SECRET"] ?: ""}\"")
-        buildConfigField("", "GENERATIVE_API_KEY", "\"${project.properties["GENERATIVE_API_KEY"] ?: ""}\"")
     }
 
     buildTypes {
--- app/src/main/java/com/idz/ChallengeZone/AskAiFragment.kt
+++ app/src/main/java/com/idz/ChallengeZone/AskAiFragment.kt
@@ -1,33 +1,25 @@
 package com.idz.ChallengeZone
 
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.idz.ChallengeZone.adapter.postAdapter.PostsRecyclerAdapter
-import com.idz.ChallengeZone.model.Model
-import com.idz.ChallengeZone.model.Post
-import androidx.fragment.app.viewModels
-import androidx.navigation.Navigation
+import android.widget.EditText
+import com.google.ai.client.generativeai.Chat
 import com.google.ai.client.generativeai.GenerativeModel
-import com.google.firebase.ktx.Firebase
+import com.google.ai.client.generativeai.type.content
 import com.idz.ChallengeZone.databinding.FragmentAskAiBinding
-import com.idz.ChallengeZone.databinding.FragmentProfileBinding
-import com.idz.ChallengeZone.model.User
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
 class AskAiFragment : Fragment() {
 
     private var binding: FragmentAskAiBinding? = null
+    lateinit var editTextInput: EditText
+    lateinit var editTextOutput: EditText
+    lateinit var chat: Chat
+    var stringBuilder: StringBuilder = java.lang.StringBuilder()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -36,32 +28,55 @@ class AskAiFragment : Fragment() {
 
         binding = FragmentAskAiBinding.inflate(inflater, container, false)
         val view = inflater.inflate(R.layout.fragment_ask_ai, container, false)
+        editTextInput = binding?.editTextInput!!
+        editTextOutput = binding?.editTextOutput!!
+        val generativeModel = GenerativeModel(
+            modelName = "gemini-1.5-pro-latest",
+            apiKey = "AIzaSyCdqlIZetWBvJ_wsJ_1Q8CIqBX20ml5bgk"
+        )
+        chat = generativeModel.startChat(
+            history = listOf(
+                content(role = "user") { text("Hello, I have 2 dogs in my house.") },
+                content(role = "model") { text("Great to meet you. What would you like to know?") }
+            )
+        )
+        stringBuilder.append("Hello, I have 2 dogs in my house.\n\n")
+        stringBuilder.append("Great to meet you. What would you like to know?\n\n")
 
-        modelCall()
+        editTextOutput.setText(stringBuilder.toString())
+        binding?.button?.setOnClickListener {
+            sendMessage()
+        }
         return binding?.root
     }
 
-    fun modelCall() {
-        var textV = binding?.demotext
-
-        val apikey = "AIzaSyCdqlIZetWBvJ_wsJ_1Q8CIqBX20ml5bgk"
-//        val generativeModel = Firebase.vertexAI.generativeModel(
-//            modelName = "gemini-2.0-flash-001", // Replace with a valid model name
-//            apiKey = apikey
-//        )
-        val generativeModel = GenerativeModel(
-            modelName = "gemini-1.5-pro-latest",
-            apiKey = apikey
-        )
-
-        val prompt = "Write a story about a magic backpack."
-//        val inputContent = content() {
-//            text("Does this look store-bought or homemade?")
-//        }
+    private fun sendMessage() {
+        stringBuilder.append(editTextInput.text.toString() + "\n\n")
+        binding?.button?.isEnabled = false // Disable the button
         MainScope().launch {
-            val response = generativeModel.generateContent(prompt)
-            textV?.text = response.text
-            Log.d("TAG", "response is ${response.toString()}")
+            try {
+                val result = chat.sendMessage(editTextInput.text.toString())
+                stringBuilder.append(result.text + "\n\n")
+                editTextOutput.setText(stringBuilder.toString())
+                editTextInput.setText("")
+            } finally {
+                binding?.button?.isEnabled = true // Re-enable the button
+            }
+        }
+    }
+
+    public fun buttonSendChat(view: View) {
+        stringBuilder.append(editTextInput.text.toString() + "\n\n")
+        binding?.button?.isEnabled = false // Disable the button
+        MainScope().launch {
+            try {
+                val result = chat.sendMessage(editTextInput.text.toString())
+                stringBuilder.append(result.text + "\n\n")
+                editTextOutput.setText(stringBuilder.toString())
+                editTextInput.setText("")
+            } finally {
+                binding?.button?.isEnabled = true // Re-enable the button
+            }
         }
     }
 }
